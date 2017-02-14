6701118107c604e02375ca5e39c5780892684861
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2017-02-14 14:48:31 -0600

    Added main menu and level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Attempting to load scene \"" + name + "\""); 
+        SceneManager.LoadScene(name); 
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game..."); 
+        Application.Quit(); 
+    }
+}
